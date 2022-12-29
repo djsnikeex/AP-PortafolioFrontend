d352245eb316ad63e60869b8cf1047f633b61996
djsnikeex <nicolasonores@hotmail.com>
2022-12-28 21:12:22 -0300

    About,Experencia and Skills

--- AP-PortafolioFr/src/app/app.module.ts
+++ AP-PortafolioFr/src/app/app.module.ts
@@ -6,17 +6,37 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './componentes/footer/footer.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { LoginComponent } from './componentes/login/login.component';
+import { ApLogoComponent } from './componentes/ap-logo/ap-logo.component';
+import { RedesComponent } from './componentes/redes/redes.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { ExperenciaComponent } from './componentes/experencia/experencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { HysComponent } from './componentes/hys/hys.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    LoginComponent
+    LoginComponent,
+    ApLogoComponent,
+    RedesComponent,
+    BannerComponent,
+    AboutComponent,
+    ExperenciaComponent,
+    EducacionComponent,
+    HysComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
